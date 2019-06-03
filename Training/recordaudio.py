d196bd96e864700fa5f19f703e9752e3618ae24b
imppppp7 <49544578+imppppp7@users.noreply.github.com>
2019-06-03 11:15:38 +0800

    inital

--- Training/recordaudio.py
+++ Training/recordaudio.py
@@ -3,16 +3,16 @@ import os
 import pyaudio
 import wave
 
-# shutil.rmtree(r'C:\Users\Administrator\Desktop\image5')
-# os.mkdir(r'C:\Users\Administrator\Desktop\image5')
+
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
+# 改变记录的时间
 RECORD_SECONDS = 10
-WAVE_OUTPUT_FILENAME = "output.wav"
+WAVE_OUTPUT_FILENAME = r"C:\Users\Administrator\Desktop\recording\output.wav"
 p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
+stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 print("* recording")
 frames = []
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
